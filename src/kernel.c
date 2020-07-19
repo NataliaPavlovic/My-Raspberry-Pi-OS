1709084f27cc9ce176006a0f903cac3250391214
NataliaP721 <natalia.pavlovic@ucalgary.ca>
2020-07-18 23:57:22 -0600

    Commented on code and got through half of EL

--- src/kernel.c
+++ src/kernel.c
@@ -1,7 +1,8 @@
+#include "printf.h"
 #include "mini_uart.h"
 #include "utils.h"
 
-int core_counter = 0;
+volatile int core_counter = 0;
 
 // It works with the Mini UART device to print to screen and read user input. 
 // The kernel just prints Hello, world! and then enters an infinite loop that 
@@ -16,10 +17,13 @@ void kernel_main(int proc_id)
 	if(0 == proc_id)
 	{
 		uart_init();
+		init_printf(0, putc);
+		int el = get_el();
+		printf("Exception level: %d \r\n", el);
 	}
 
 	uart_send_string("Hello, from processor ");
-	uart_send((char) proc_id+48);
+	uart_send((char) proc_id+'0');
 	uart_send_string("\r\n");
 
 	// uart_send_string("Hello, world!\r\n");
